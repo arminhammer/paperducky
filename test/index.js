d0ef314c62c494e5e12d64183deda1df413e91f5
arminhammer <arminhammer@gmail.com>
2015-11-14 18:32:31 -0500

    Init command completed.

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var assert = require('assert');
-var paperducky = require('../lib');
+//var paperducky = require('../lib');
 
-describe('paperducky', function () {
-  it('should have unit test!', function () {
+describe('paperducky', function() {
+  it('should have unit test!', function() {
     assert(false, 'we expected this package author to add actual unit tests.');
   });
 });
